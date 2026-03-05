240eb81f6029a38aeef85374f40623e928dd0901
Babs90000 <b.camara.diaby@outlook.com>
2026-03-05 17:58:10 +0000

    fix: import index.css dans main.tsx pour fond sombre

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import TodoInterface from "./components/TodoList/TodoInterface";
-import TodoList from "./components/TodoList/TodoInterface";
 
 export default function App() {
   return <TodoInterface />;
